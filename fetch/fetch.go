d25e8b38d920dafbbc0b18716e43e9e30b9a7353
Mike Metral <metral@gmail.com>
2014-08-12 19:29:30 -0700

    Remove sort (etcd does it) & use discovery_url flag

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -4,29 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
-	"sort"
+	"strings"
 
 	"github.com/coreos/go-etcd/etcd"
 )
-
-type NodeGroup []*etcd.Node //NodeGroup is a slice of pointers to etcd Nodes
-
-// Sort Interface implementation methods
-func (n NodeGroup) Len() int {
-	return len(n)
-}
-
-func (n NodeGroup) Less(i, j int) bool {
-	if n[i].Key < n[j].Key {
-		return true
-	}
-	return false
-}
-
-func (n NodeGroup) Swap(i, j int) {
-	n[i], n[j] = n[j], n[i]
-}
 
 func Usage() {
 	fmt.Printf("Usage: %s\n", os.Args[0])
@@ -35,13 +18,20 @@ func Usage() {
 }
 
 func SetupFlags() (discoveryHost, discoveryPath *string) {
-	discoveryHost = flag.String("discovery_host",
-		"http://127.0.0.1:4001", "Discovery URL:Port")
-	discoveryPath = flag.String("discovery_path",
-		"",
-		"Discovery path i.e. _etcd/registry/uVa2GHOTTxl27eyKk6clBwyaurf7KiWd")
-
+	discoveryURL := flag.String("discovery_url", "", "Discovery URL")
 	flag.Parse()
+
+	u, err := url.Parse(*discoveryURL)
+	if err != nil {
+		panic(err)
+	}
+
+	discoveryHost = new(string)
+	*discoveryHost = u.Scheme + "://" + u.Host
+
+	path := strings.Split(u.Path, "/keys/")[1]
+	discoveryPath = new(string)
+	*discoveryPath = path
 
 	if *discoveryHost == "" || *discoveryPath == "" {
 		Usage()
@@ -55,19 +45,10 @@ func main() {
 	discoveryHost, discoveryPath := SetupFlags()
 
 	client := etcd.NewClient([]string{*discoveryHost})
-	resp, _ := client.Get(*discoveryPath, false, false)
+	resp, _ := client.Get(*discoveryPath, true, false)
 
 	// Store the pointer to the etcd nodes as a NodeGroup
-	group := NodeGroup{}
 	for _, n := range resp.Node.Nodes {
-		group = append(group, n)
-	}
-
-	// Sort the NodeGroup
-	sort.Sort(group)
-
-	// Print out sorted NodeGroup by key
-	for _, n := range group {
 		log.Printf("%s: %s\n", n.Key, n.Value)
 	}
 }
