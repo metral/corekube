a5949180e13fb5a7183d0bf6e12cc5532c19f301
Mike Metral <metral@gmail.com>
2014-08-13 01:36:40 -0700

    Fix pointer issues & remove dep for ADD in dockerfile

--- etcd_nodes/etcd_nodes.go
+++ etcd_nodes/etcd_nodes.go
@@ -10,9 +10,9 @@ import (
 	"github.com/coreos/go-etcd/etcd"
 )
 
-func ParseDiscovery() (discoveryHost, discoveryPath *string) {
+func ParseDiscovery() (discoveryHost, discoveryPath string) {
 	// Pull the ETCD_DISCOVERY env var and parse it for the etcd client usage
-	file := "/gopath/src/app/20-cloudinit.conf"
+	file := "/mnt/20-cloudinit.conf"
 	cmd := fmt.Sprintf("cat %s | grep ETCD_DISCOVERY | cut -d '=' -f 3 | cut -d '\"' -f 1", file)
 	out, err := exec.Command("sh", "-c", cmd).Output()
 
@@ -23,12 +23,11 @@ func ParseDiscovery() (discoveryHost, discoveryPath *string) {
 		panic(err)
 	}
 
-	discoveryHost = new(string)
-	*discoveryHost = u.Scheme + "://" + u.Host
+	discoveryHost = fmt.Sprintf("%s://%s", u.Scheme, u.Host)
 
 	path := strings.Split(u.Path, "/keys/")[1]
-	discoveryPath = new(string)
-	*discoveryPath = path
+	path = strings.Replace(path, "\n", "", -1)
+	discoveryPath = fmt.Sprintf("%s", path)
 
 	return discoveryHost, discoveryPath
 }
@@ -37,8 +36,8 @@ func main() {
 	discoveryHost, discoveryPath := ParseDiscovery()
 
 	// Connect to the etcd discovery to pull the nodes
-	client := etcd.NewClient([]string{*discoveryHost})
-	resp, _ := client.Get(*discoveryPath, true, false)
+	client := etcd.NewClient([]string{discoveryHost})
+	resp, _ := client.Get(discoveryPath, true, false)
 
 	for _, n := range resp.Node.Nodes {
 		log.Printf("%s: %s\n", n.Key, n.Value)
