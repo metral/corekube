a1d6dd10ceb05158fa27ca5b863a1d4e3b0162a1
Mike Metral <metral@gmail.com>
2014-08-14 10:11:02 -0700

    Change name to etcd_nodes

--- misc/build_run.sh
+++ misc/build_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=`docker build --rm -t corekube ../etcd_nodes/.`
+result=`docker build --rm -t etcd_nodes ../etcd_nodes/.`
 echo "$result"
 
 echo ""
@@ -10,5 +10,5 @@ echo ""
 build_status=`echo $result | grep "Successfully built"`
 
 if [ "$build_status" ] ; then
-    docker run corekube
+    docker run etcd_nodes
 fi
