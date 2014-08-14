245ae713d681512b8b61068f33c512aade1336be
Mike Metral <metral@gmail.com>
2014-08-14 00:07:09 -0700

    Cleanup, replace go-etcd, save machine data
    - Add comments
    - Replace go-etcd for regular HTTP GET Request since go-etcd was having random
      error: "501: All the given peers are not reachable (Tried to connect to each
      peer twice and failed) [0]"
    - Create custom structs to store machine information

--- etcd_nodes/etcd_nodes.go
+++ etcd_nodes/etcd_nodes.go
@@ -3,56 +3,122 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
 	"os/exec"
-
-	"github.com/coreos/go-etcd/etcd"
+	"strings"
 )
 
 type KeyValue map[string]interface{}
 
 type KeyValueGroup []KeyValue // a slice of KeyValue's
 
+/* i.e. KeyValueGroup composed of KeyValue's
+[
+	{
+		"name":"abc",
+		"state":"follower",
+		"clientURL":"http://1.2.3.4:4001",
+		"peerURL":"http://1.2.3.4:7001"
+	},
+	...
+]
+*/
+
+type EtcdMachine struct {
+	name      string
+	state     string
+	clientURL string
+	peerURL   string
+}
+
+type EtcdMachineGroup []EtcdMachine // a slice of EtcdMachine's
+
+// Modify EtcdMachine
+func (e *EtcdMachine) SetProperties(machineData KeyValue) {
+	e.name = machineData["name"].(string)
+	e.state = machineData["state"].(string)
+	e.clientURL = machineData["clientURL"].(string)
+	e.peerURL = machineData["peerURL"].(string)
+}
+
+// Retrieve string of EtcdMachine
+func (e *EtcdMachine) String() string {
+	output := fmt.Sprintf("Name: %s | State: %s | ClientURL: %s | PeerURL: %s",
+		e.name,
+		e.state,
+		e.clientURL,
+		e.peerURL,
+	)
+	return output
+}
+
+// Check for errors and panic, if found
 func checkForErrors(err error) {
 	if err != nil {
-		panic(err)
-		os.Exit(2)
+		log.Fatal("%s", err)
 	}
 }
 
+// Get the IP address of the docker host as this is run from within container
 func getDockerHostIP() (ip string) {
 	cmd := fmt.Sprintf("netstat -nr | grep '^0\\.0\\.0\\.0' | awk '{print $2}'")
 	out, err := exec.Command("sh", "-c", cmd).Output()
 	checkForErrors(err)
 
-	return string(out)
+	ip = string(out)
+	ip = strings.Replace(ip, "\n", "", -1)
+	return ip
 }
 
-func getDiscoveryHost(host string, port string) (discoveryHost string) {
+// Compose the etcd API host:port location
+func getEtcdAPI(host string, port string) (etcdAPI string) {
 	return fmt.Sprintf("http://%s:%s", host, port)
 }
 
+func httpGetRequest(url string) (content []byte) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+
+	return body
+}
+
+// Access the CoreOS / docker etcd API to extract machine information
 func main() {
 	// Local etcd API host & port
 	port := "7001"
-	discoveryHost := getDiscoveryHost(getDockerHostIP(), port)
+	etcdAPI := getEtcdAPI(getDockerHostIP(), port)
 
 	// Request path listing etcd machines in cluster
-	discoveryPath := "admin/machines"
+	etcdAPIPath := "v2/admin/machines"
+	url := fmt.Sprintf("%s/%s", etcdAPI, etcdAPIPath)
 
-	// Connect & send request to local etcd API to retrieve machines JSON
-	client := etcd.NewClient([]string{discoveryHost})
-	req := etcd.NewRawRequest("GET", discoveryPath, nil, nil)
-	rawRespPtr, err := client.SendRequest(req)
-	checkForErrors(err)
-	jsonResponse := rawRespPtr.Body
+	jsonResponse := httpGetRequest(url)
 
-	var machines KeyValueGroup
-	err = json.Unmarshal(jsonResponse, &machines)
+	// Decode the JSON returned
+	var machineDataGroup KeyValueGroup
+	err := json.Unmarshal(jsonResponse, &machineDataGroup)
 	checkForErrors(err)
+
+	// Use machine data to create local objects of the etcd machines
+	machines := EtcdMachineGroup{}
+	for _, machineData := range machineDataGroup {
+		machine := EtcdMachine{}
+		machine.SetProperties(machineData)
+		machines = append(machines, machine)
+	}
 
 	for _, machine := range machines {
-		log.Printf("%s -- %s -- %s -- %s\n", machine["name"], machine["state"], machine["clientURL"], machine["peerURL"])
+		log.Printf("%s\n", machine.String())
 	}
 }
