81e5ab348f3a9762ae76652a17e415402987766b
Mike Metral <metral@gmail.com>
2014-08-18 23:01:54 -0700

    Add branching ability

--- misc/build_run.sh
+++ misc/build_run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-result=`docker build --rm -t etcd_nodes ../etcd_nodes/.`
+EXPECTEDARGS=2
+if [ $# -lt $EXPECTEDARGS ]; then
+    echo "Usage: $0 <DOCKER_REPO_TAG> <NUMBER OF EXPECTED MACHINES>"
+    exit 0
+fi
+
+PARENTDIR="$(dirname `pwd`)"
+
+BRANCH=$1
+MACHINE_COUNT=$2
+
+if [ "$BRANCH" != "latest" ]; then
+    git checkout -b $BRANCH origin/$BRANCH
+fi
+
+result=`docker build --rm -t etcd_nodes $PARENTDIR/etcd_nodes/.`
 echo "$result"
 
 echo ""
@@ -10,5 +25,5 @@ echo ""
 build_status=`echo $result | grep "Successfully built"`
 
 if [ "$build_status" ] ; then
-    docker run -e DOCKERHOST_HOSTNAME=`hostname` etcd_nodes --machine_count=$1
+    docker run -e DOCKERHOST_HOSTNAME=`hostname` etcd_nodes --machine_count=$MACHINE_COUNT
 fi
