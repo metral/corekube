4b93211753bef9c8c844e7468dc1e2ff4c55197e
Mike Metral <metral@gmail.com>
2014-12-11 18:59:40 -0800

    Do curl in build_run

--- build_run.sh
+++ build_run.sh
@@ -19,6 +19,8 @@ echo ""
 
 build_status=`echo $result | grep "Successfully built"`
 
+/usr/bin/curl -L http://localhost:4001/v2/keys/deployed -XPUT -d value="[]"
+
 if [ "$build_status" ] ; then
     docker run -v /tmp:/units -v $DIR/overlord/unit_templates:/templates overlord:$BRANCH
 fi
