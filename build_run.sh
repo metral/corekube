bce8fc0a540084e3b7c26bd43690be24759e45bc
Mike Metral <metral@gmail.com>
2014-12-21 20:00:41 -0800

    Remove branch from build_run

--- build_run.sh
+++ build_run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-EXPECTEDARGS=1
-if [ $# -lt $EXPECTEDARGS ]; then
-    echo "Usage: $0 <BRANCH>"
-    exit 0
-fi
-
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-BRANCH=$1
-
-result=`docker build --rm -t overlord:$BRANCH $DIR/overlord/.`
+result=`docker build --rm -t overlord $DIR/overlord/.`
 echo "$result"
 
 echo ""
@@ -20,5 +12,5 @@ echo ""
 build_status=`echo $result | grep "Successfully built"`
 
 if [ "$build_status" ] ; then
-    docker run -d -v /tmp:/units -v $DIR/overlord/unit_templates:/templates overlord:$BRANCH
+    docker run -d -v /tmp:/units -v $DIR/overlord/unit_templates:/templates overlord
 fi
