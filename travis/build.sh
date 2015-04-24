c9dc3544428833ccbffdefd893b1b20cef20b8c0
Mike Metral <metral@gmail.com>
2015-04-24 12:40:11 -0700

    Add commit used

--- travis/build.sh
+++ travis/build.sh
@@ -4,5 +4,6 @@ go get github.com/tools/godep
 
 git clone https://github.com/metral/corekube_travis
 pushd corekube_travis/corekube_test
+echo "corekube_travis commit: `git rev-parse --short HEAD`"
 godep get ./...
 popd
