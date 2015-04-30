c3dcb18edc3237d7eac8d42d0b0338e817d1ea9b
Mike Metral <metral@gmail.com>
2015-04-30 12:27:20 -0700

    Formatting fixes

--- travis/build.sh
+++ travis/build.sh
@@ -4,7 +4,9 @@ go get github.com/tools/godep
 
 git clone https://github.com/metral/corekube_travis
 pushd corekube_travis/corekube_test
+echo "========================================"
 echo "corekube_travis commit: `git rev-parse --short HEAD`"
+echo "========================================"
 godep get ./...
 popd
 
