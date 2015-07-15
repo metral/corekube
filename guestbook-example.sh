3531ea885faa0aff6c9ad076ecae43a8e48343fb
Mike Metral <metral@gmail.com>
2015-07-15 14:27:01 -0700

    Change guestbook extensions

--- guestbook-example.sh
+++ guestbook-example.sh
@@ -6,9 +6,9 @@ git clone https://github.com/GoogleCloudPlatform/kubernetes
 pushd kubernetes
 git checkout -b v$VERSION tags/v$VERSION
 
-/opt/bin/kubectl create -f examples/guestbook/redis-master-controller.json
-/opt/bin/kubectl create -f examples/guestbook/redis-master-service.json
-/opt/bin/kubectl create -f examples/guestbook/redis-slave-controller.json
-/opt/bin/kubectl create -f examples/guestbook/redis-slave-service.json
-/opt/bin/kubectl create -f examples/guestbook/frontend-controller.json
+/opt/bin/kubectl create -f examples/guestbook/redis-master-controller.yaml
+/opt/bin/kubectl create -f examples/guestbook/redis-master-service.yaml
+/opt/bin/kubectl create -f examples/guestbook/redis-slave-controller.yaml
+/opt/bin/kubectl create -f examples/guestbook/redis-slave-service.yaml
+/opt/bin/kubectl create -f examples/guestbook/frontend-controller.yaml
 /opt/bin/kubectl get pods
