15511c993740f5a3962edb40731361b225eb0671
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2015-07-22 16:03:05 -0700

    Create frontend service

--- guestbook-example.sh
+++ guestbook-example.sh
@@ -11,4 +11,5 @@ git checkout -b v$VERSION tags/v$VERSION
 /opt/bin/kubectl create -f examples/guestbook/redis-slave-controller.yaml
 /opt/bin/kubectl create -f examples/guestbook/redis-slave-service.yaml
 /opt/bin/kubectl create -f examples/guestbook/frontend-controller.yaml
+/opt/bin/kubectl create -f examples/guestbook/frontend-service.yaml
 /opt/bin/kubectl get pods
